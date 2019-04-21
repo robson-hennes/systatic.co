dc44cef833cc9969c1db479ef2b03a60071470b0
Duncan McClean <duncan@mcclean.co.uk>
2019-04-21 17:42:36 +0100

    Intravert ads

--- views/layouts/app.blade.php
+++ views/layouts/app.blade.php
@@ -8,7 +8,9 @@
     <div id="app">
         @include('partials.header')
         @yield('body')
+        @include('partials.footer')
     </div>
     <script src="/js/app.js"></script>
+    <script defer src="https://intravert.co/serve/334a34aec6.78.js"></script>
 </body>
 </html>
---
+++ views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="mt-8 mb-6">
+    <div class="container mx-auto">
+        <div class="intravert-space" id="space-334a34aec678"></div>
+    </div>
+</footer>
