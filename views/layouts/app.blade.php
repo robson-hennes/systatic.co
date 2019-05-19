7c0d06aaf66534a885f22e3fdccd8148a1520322
Duncan McClean <damcclean@users.noreply.github.com>
2019-05-19 17:45:34 +0100

    Update app.blade.php

--- views/layouts/app.blade.php
+++ views/layouts/app.blade.php
@@ -3,6 +3,9 @@
 <head>
     <title>{{ $title }}</title>
     <link rel="stylesheet" href="/css/app.css">
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 </head>
 <body class="text-base font-normal font-sans bg-white text-black leading-loose">
     <div id="app">
