6e71e477629766936fe7c313f4fe218d515bd232
Duncan McClean <duncan@mcclean.co.uk>
2019-06-14 19:06:35 +0100

    wip

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,8 +2,6 @@ let mix = require('laravel-mix');
 let tailwindcss = require('tailwindcss');
 
 mix.js('js/app.js', 'dist/js')
-   .sass('sass/app.scss', 'dist/css')
-     .options({
-       processCssUrls: false,
-       postCss: [ tailwindcss('./tailwind.js') ],
-     });
+   .postCss('css/app.css', 'dist/css', [
+     require('tailwindcss')
+   ]);
