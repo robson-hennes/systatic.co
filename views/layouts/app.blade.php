8b3e75e1c64d422f76cc74d15705065a27ac8494
Duncan McClean <duncan@mcclean.co.uk>
2019-06-14 21:00:10 +0100

    added scripts

--- views/layouts/app.blade.php
+++ views/layouts/app.blade.php
@@ -5,7 +5,8 @@
     <link rel="stylesheet" href="/css/app.css">
     <meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @include('partials.extras.scripts')
 </head>
 <body class="text-gray-700 bg-white leading-loose font-normal text-base">
     <div id="app">
