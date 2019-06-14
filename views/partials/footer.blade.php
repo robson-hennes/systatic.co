35d91648eeb1a27b98555ee6dd14e1c19c662472
Duncan McClean <duncan@mcclean.co.uk>
2019-06-14 20:22:13 +0100

    footer

--- views/partials/footer.blade.php
+++ views/partials/footer.blade.php
@@ -1,5 +1,6 @@
-<footer class="mt-8 mb-6">
-    <div class="container mx-auto">
+<footer class="bg-gray-300 p-8">
+    <div class="container mx-auto flex flex-col md:flex-row md:items-center md:justify-between">
+        <p>Systatic is an open-source project, started by <a class="font-semibold" href="http://duncan.mcclean.co.uk">Duncan McClean</a>.</p>
         <div class="intravert-space" id="space-334a34aec678"></div>
     </div>
 </footer>
