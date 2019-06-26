bc0503ab31b8f506a7af214b395ea4d6879d0964
Duncan McClean <damcclean@users.noreply.github.com>
2019-06-26 20:52:21 +0100

    Replaced Fathom

--- views/partials/extras/scripts.blade.php
+++ views/partials/extras/scripts.blade.php
@@ -1,15 +1,1 @@
-<!-- Fathom - simple website analytics - https://github.com/usefathom/fathom -->
-<script>
-(function(f, a, t, h, o, m){
-	a[h]=a[h]||function(){
-		(a[h].q=a[h].q||[]).push(arguments)
-	};
-	o=f.createElement('script'),
-	m=f.getElementsByTagName('script')[0];
-	o.async=1; o.src=t; o.id='fathom-script';
-	m.parentNode.insertBefore(o,m)
-})(document, window, '//stats.iamlittle.co.uk/tracker.js', 'fathom');
-fathom('set', 'siteId', 'PDYUU');
-fathom('trackPageview');
-</script>
-<!-- / Fathom -->
+<script> window.siteId = '5d13ccd10c5bb'; </script> <script src="https://bob.iamlittle.co.uk/script.js"></script>
