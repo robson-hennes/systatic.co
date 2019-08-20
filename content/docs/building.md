cf411231ec3bcaf64f4904f0fe1c9d2ad73b4471
Duncan McClean <duncan@mcclean.co.uk>
2019-08-20 21:24:42 +0100

    I got carried away with front-end magic

--- content/docs/building.md
+++ content/docs/building.md
@@ -3,11 +3,10 @@ title: Building
 description: Learn how to build your site for development or production.
 ---
 
-Whenever you make changes to any of your views, your content or configuration, you'll want to rebuild your site.
+When you want to build your site, you can use the following command:
 
+```bash
+$ php systatic build
 ```
-php systatic build
-```
 
-Systatic will generate HTML files and put them in your output directory, usually `dist` (it can be changed in your config file).
-
+It will generate HTML files in your output directory (by default, `dist`).
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,24 @@
 let mix = require('laravel-mix');
-let tailwindcss = require('tailwindcss');
 require('laravel-mix-purgecss');
 
 mix.js('src/js/app.js', 'dist/js')
    .postCss('src/css/app.css', 'dist/css', [
-     require('tailwindcss')
-   ]);
+     require('tailwindcss'),
+     require('postcss-nested')
+   ])
+   .browserSync({
+   		proxy: 'systatic-site.test',
+   		files: [
+   			'./content/*/*.md',
+   			'./src/js/*.js',
+   			'./src/css/*.css',
+   			'./views/*.blade.php',
+   			'./views/*/*.blade.php'
+   		]
+   });
 
 if (mix.inProduction()) {
     mix.purgeCss({
-        folders: ['views', 'src', 'content']
+        folders: ['content', 'src', 'views']
     });
 }
