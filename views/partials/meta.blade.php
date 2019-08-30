b5a21ac065a355ad3a88593c63f9c92cf1c38dac
Duncan McClean <duncan@mcclean.co.uk>
2019-08-30 17:24:26 +0100

    fixed site issues

--- views/partials/meta.blade.php
+++ views/partials/meta.blade.php
@@ -1,16 +1,16 @@
 <!-- Primary Meta Tags -->
 <title>{{ $title }} | Systatic</title>
 <meta name="title" content="{{ $title }} | Systatic">
-<meta name="description" content="{{ $description }}">
+<meta name="description" content="@if(isset($description)) {{ $description }} @else Last updated on {{ $last_updated }}. @endif">
 
 <!-- Open Graph / Facebook -->
 <meta property="og:type" content="website">
 <meta property="og:url" content="https://systatic.co/{{ $permalink }}">
 <meta property="og:title" content="{{ $title }} | Systatic">
-<meta property="og:description" content="{{ $description }}">
+<meta property="og:description" content="@if(isset($description)) {{ $description }} @else Last updated on {{ $last_updated }}. @endif">
 
 <!-- Twitter -->
 <meta property="twitter:card" content="summary_large_image">
 <meta property="twitter:url" content="https://systatic.co/{{ $permalink }}">
 <meta property="twitter:title" content="{{ $title }} | Systatic">
-<meta property="twitter:description" content="{{ $description }}">
+<meta property="twitter:description" content="@if(isset($description)) {{ $description }} @else Last updated on {{ $last_updated }}. @endif">
